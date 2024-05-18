fcd48c6a18eb2db50959ac2ac17a9c0069b45766
Nhincaije <nhinjee@gmail.com>
2024-05-19 00:18:47 +0700

    Initial commit

---
+++ Framework/Patch/GameServerPatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using MultiplayerMod.Framework.Network;
+using StardewValley.Network;
+using LidgrenServer = MultiplayerMod.Framework.Network.LidgrenServer;
+
+namespace MultiplayerMod.Framework.Patch
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    internal class GameServerPatch : IPatch
+    {
+        private readonly Type PatchType = typeof(GameServer);
+        public void Apply(Harmony harmonyInstance)
+        {
+            harmonyInstance.Patch(AccessTools.Constructor(PatchType, new Type[] { typeof(bool) }), postfix: new HarmonyMethod(AccessTools.Method(this.GetType(), nameof(Postfix_contruction))));
+        }
+        private static void Postfix_contruction(GameServer __instance)
+        {
+            List<Server> servers = ModUtilities.Helper.Reflection.GetField<List<Server>>(__instance, "servers").GetValue();           
+            LidgrenServer lidgrenServer = new LidgrenServer(__instance);
+            servers.Add(ModUtilities.multiplayer.InitServer(lidgrenServer));
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
---
+++ Framework/Patch/Mobile/Game1Patch.cs
@@ -0,0 +1,67 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerMod.Framework.Patch.Mobile
+{
+    public class Game1Patch : IPatch
+    {
+        public static readonly Type PATCH_TYPE = typeof(Game1);
+        public static IReflectedField<int> xEdgeField;
+        public static IReflectedField<string> savesPathField;
+        public static IReflectedField<Texture2D> mobileSpriteSheetField;
+        public Game1Patch()
+        {
+            xEdgeField = ModUtilities.Helper.Reflection.GetField<int>(PATCH_TYPE, "xEdge");
+            savesPathField = ModUtilities.Helper.Reflection.GetField<string>(PATCH_TYPE, "savesPath");
+            mobileSpriteSheetField = ModUtilities.Helper.Reflection.GetField<Texture2D>(PATCH_TYPE, "mobileSpriteSheet");
+        }
+
+
+        public static Texture2D mobileSpriteSheet
+        {
+            get
+            {
+                return mobileSpriteSheetField.GetValue();
+            }
+            set { mobileSpriteSheetField.SetValue(value); }
+        }
+
+        public static int xEdge
+        {
+            get
+            {
+                return xEdgeField.GetValue();
+            }
+            set
+            {
+                xEdgeField.SetValue(value);
+            }
+        }
+        public static string savesPath
+        {
+            get
+            {
+                return savesPathField.GetValue();
+            }
+            set
+            {
+                savesPathField.SetValue(value);
+            }
+        }
+
+        public void Apply(Harmony harmony)
+        {
+             
+        }
+        
+    }
+}
